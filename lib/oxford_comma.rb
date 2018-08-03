438f188bb58368fc398db00157b73d8e172ae6c6
Yixuan Wang <ywang0506@gmail.com>
2018-08-02 21:08:47 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+  if array.length == 1
+    str = array.join
+  elsif array.length == 2
+    array[-1] = "and " + array[-1]
+    str = array.join(" ")
+  else
 
+    array[-1] = "and " + array[-1]
+    str = array.join(", ")
+    str
+  end  
 end
